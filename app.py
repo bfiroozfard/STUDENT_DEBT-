db8c525f144e31ad2538820009ffb92951a20c50
bfiroozfard <bfiroozfard@gmail.com>
2020-01-12 02:32:05 -0800

    add graphs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template
 from bson.json_util import dumps
+import pandas as pd
 
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
 import pymongo
@@ -27,36 +28,114 @@ def index():
     # Return the template with the teams list passed in
     return render_template('index.html')
 
-@app.route("/test")
-def test():
-    data = [{
-        "x": [1, 2, 3, 4, 5],
-        "y": [1, 2, 4, 8, 16]}]
 
-    return jsonify(data)
-    
-@app.route("/byage")
-def byage():
-    data = [{
-        "x": [1, 2, 3, 4, 5],
-        "y": [1, 2, 4, 8, 16]}]
-
-    return jsonify(data)
     
 @app.route("/bysize")
 def bysize():
-    bysize = list(db.by_size.find())
-    print(bysize)
-    result = dumps(bysize)
-    return render_template('index.html', bysize=result)
+    bysize= db.by_size1.find()
+    df3 = pd.DataFrame(bysize)
+    df3['Outstanding']=pd.to_numeric(df3['Outstanding'])
+    df3['Borrowers']=pd.to_numeric(df3['Borrowers'])
+    my_year=list(df3["year"].unique())
+    my_range=list(df3["loan_range"].unique())
+    datab=[]
+    for  range in my_range:
+        datab.append({"x":my_year,"y":list(df3.loc[df3["loan_range"]==range]["Borrowers"]),"name":range,"type":"bar"})
+    print(datab)
+
+    return jsonify(datab)  
+
+@app.route("/bysizeb")
+def bysizeb():
+    bysize= db.by_size1.find()
+    df3 = pd.DataFrame(bysize)
+    df3['Outstanding']=pd.to_numeric(df3['Outstanding'])
+    df3['Borrowers']=pd.to_numeric(df3['Borrowers'])
+    my_year=list(df3["year"].unique())
+    my_range=list(df3["loan_range"].unique())
+    datab=[]
+    for  range in my_range:
+        datab.append({"x":my_year,"y":list(df3.loc[df3["loan_range"]==range]["Outstanding"]),"name":range,"type":"bar"})
+    print(datab)
+
+    return jsonify(datab)      
+
+@app.route("/byage")
+def byage():
+    byage= db.by_age1.find()
+    df2 = pd.DataFrame(byage)
+    df2["Dollars_Outstanding"] = pd.to_numeric(df2["Dollars_Outstanding"])
+    df2["Borrowers "]= pd.to_numeric(df2["Borrowers "])
+    my_year=list(df2["year"].unique())
+    my_range=list(df2["age_range"].unique())
+    datab=[]
+    for  range in my_range:
+        datab.append({"x":my_year,"y":list(df2.loc[df2["age_range"]==range]["Borrowers "]),"name":range,"type":"bar"})
+    print(datab)
+
+
+
+    return jsonify(datab) 
+
+@app.route("/byageb")
+def byage2():
+    byage2= db.by_age1.find()
+    df2 = pd.DataFrame(byage2)
+    df2["Dollars_Outstanding"] = pd.to_numeric(df2["Dollars_Outstanding"])
+    df2["Borrowers "]= pd.to_numeric(df2["Borrowers "])
+    my_year=list(df2["year"].unique())
+    my_range=list(df2["age_range"].unique())
+    datab=[]
+    for  range in my_range:
+        datab.append({"x":my_year,"y":list(df2.loc[df2["age_range"]==range]["Dollars_Outstanding"]),"name":range,"type":"bar"})
+    print(datab)
+
+
+
+    return jsonify(datab)  
 
 
 @app.route("/bylocation")
 def bystate():
     bylocation = list(db.by_location1.find())
-	
-    result = dumps(bylocation)
-    return result
     
+    df=pd.DataFrame(bylocation)
+    df["Borrowers"] = pd.to_numeric(df["Borrowers"])
+    df["Balance"] = pd.to_numeric(df["Balance"])
+    df1=df.sort_values("Borrowers",inplace=False,ascending=False)
+    location=list(df1["Location"])
+    borrower=list(df1["Borrowers"])
+
+
+
+    trace = {
+        "x":location ,
+        "y": borrower,
+        "type": "bar"
+    }
+    print(trace)
+    return (trace)    
+   
+@app.route("/bylocationb")
+def bystate1():
+    bylocation = list(db.by_location1.find())
+    
+    df=pd.DataFrame(bylocation)
+    df["Borrowers"] = pd.to_numeric(df["Borrowers"])
+    df["Balance"] = pd.to_numeric(df["Balance"])
+    df1=df.sort_values("Borrowers",inplace=False,ascending=False)
+    location=list(df1["Location"])
+    borrower=list(df1["Borrowers"])
+    balance=list(df1["Balance"])
+
+
+
+    trace = {
+        "x":location ,
+        "y": balance,
+        "type": "bar"
+    }
+    print(trace)
+    return (trace)     
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,16 @@
+function ShowGraph1(value){
+	var yType=value;
+	var xType=document.getElementById("loanTypeDropDown").value
+  if (xType){
+  	ShowGraph(xType,yType)
+	}
 
+}
 
-function ShowGraph(value){
+function ShowGraph(value,yy){
 	// alert(value);
 	var url;
+	if( yy=="borrower"){
 	switch(value) {
 	  case "age":
 		url = "/byage";
@@ -19,46 +27,69 @@ function ShowGraph(value){
 	  default:
 		url = "/byage";
 	}
+	}
+if(yy=="balance"){
+	switch(value) {
+	  case "age":
+		url = "/byageb";
+		getAgeChart(url);
+		break;
+	  case "size":
+		url = "/bysizeb";
+		getSizeChart(url);
+		break;
+	  case "location":
+		url = "/bylocationb";
+		getLocationChart(url);
+		break;
+	  default:
+		url = "/byage";
+	}
+	}
 
 };
-var year=[];
-var borrowers=[];
-var amount =[];
-
-
-//const map1 = array1.map(x => x * 2);
 
 function getLocationChart(url){
-	var state=[];
-	var balance=[];
 	d3.json(url).then(function(response){
-		response.map(resp=>state.push(resp.Location));
-		response.map(resp=>balance.push(Number(resp.Balance)));
-		console.log(response[0]);
-		});
-	console.log(state);
-	console.log(balance);
+		 console.log(url);
+		 
 
-	var data = [
-  {
-    x:state,
-    y:balance,
-    type: 'bar'
-  }
-];
-
-Plotly.newPlot("by_location", data);
-
-		
+	var data = [response]
 
 
+	Plotly.newPlot('plot', data);	
+	});
+}
+
+function getAgeChart(url){
+	d3.json(url).then(function(response){
+		console.log(response);
+	var data = response;	
+	var layout = {barmode: 'group'};
+	Plotly.newPlot('plot', data, layout);
 	
-	};
+	});
+}
+function getSizeChart(url){
+	d3.json(url).then(function(response){
+		console.log(response);
+	var data = response;	
+	var layout = {barmode: 'group'};
+	Plotly.newPlot('plot', data, layout);
 	
+	});
 	
+}
 
-function getAgeChart(url){};
-	
 
-function getSizeChart(url){};
-	
+function init() {
+  // Grab a reference to the dropdown select element
+  var yType = document.getElementById("outputType").value
+  console.log(yType)
+  
+}
+
+
+
+// Initialize the dashboard
+init();
