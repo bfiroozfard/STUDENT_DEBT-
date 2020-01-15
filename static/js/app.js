099f931cfcc59cac42ea25e8e377ed849ad63511
bfiroozfard <bfiroozfard@gmail.com>
2020-01-14 20:41:19 -0800

    for presentation

--- static/js/app.js
+++ static/js/app.js
@@ -1,95 +1,49 @@
 function ShowGraph1(value){
 	var yType=value;
 	var xType=document.getElementById("loanTypeDropDown").value
-  if (xType){
-  	ShowGraph(xType,yType)
+	if (xType){
+		ShowGraph(xType,yType)
 	}
-
 }
 
-function ShowGraph(value,yy){
-	// alert(value);
+function ShowGraph(value, outputType){
 	var url;
-	if( yy=="borrower"){
 	switch(value) {
 	  case "age":
-		url = "/byage";
-		getAgeChart(url);
+		url = outputType == "borrower" ? "/byage": "/byageb";
 		break;
 	  case "size":
-		url = "/bysize";
-		getSizeChart(url);
+		url = outputType == "borrower" ? "/bysize" : "/bysizeb";		
 		break;
 	  case "location":
-		url = "/bylocation";
-		getLocationChart(url);
+		url = outputType == "borrower" ? "/bylocation" : "/bylocationb";
+		//getLocationChart(url);
 		break;
-	  default:
-		url = "/byage";
 	}
-	}
-if(yy=="balance"){
-	switch(value) {
-	  case "age":
-		url = "/byageb";
-		getAgeChart(url);
-		break;
-	  case "size":
-		url = "/bysizeb";
-		getSizeChart(url);
-		break;
-	  case "location":
-		url = "/bylocationb";
-		getLocationChart(url);
-		break;
-	  default:
-		url = "/byage";
-	}
-	}
-
+	getChart(url, value);
+		
 };
-
-function getLocationChart(url){
+function getChart(url, chartType){
 	d3.json(url).then(function(response){
-		 console.log(url);
-		 
+		console.log(url); 
+		var data = chartType=="location" ? [response]: response;
+		var layout =chartType=="age" ? {barmode: 'group'} : null;
 
-	var data = [response]
+		Plotly.newPlot('plot', data, layout);	
+		
+		var loanType = document.getElementById("loanTypeDropDown").options[document.getElementById("loanTypeDropDown").selectedIndex].text;
+		var outputType = document.getElementById("outputType").options[document.getElementById("outputType").selectedIndex].text;
+		
+		document.getElementById("chartResult").innerHTML = `Chart Result for ${loanType} and ${outputType}`;
 
-
-	Plotly.newPlot('plot', data);	
-	});
+	});		
 }
-
-function getAgeChart(url){
-	d3.json(url).then(function(response){
-		console.log(response);
-	var data = response;	
-	var layout = {barmode: 'group'};
-	Plotly.newPlot('plot', data, layout);
-	
-	});
-}
-function getSizeChart(url){
-	d3.json(url).then(function(response){
-		console.log(response);
-	var data = response;	
-	var layout = {barmode: 'group'};
-	Plotly.newPlot('plot', data, layout);
-	
-	});
-	
-}
-
 
 function init() {
   // Grab a reference to the dropdown select element
   var yType = document.getElementById("outputType").value
-  console.log(yType)
-  
+  console.log(yType)  
 }
-
-
 
 // Initialize the dashboard
 init();
